04e09c8c9e0d07be2ba17b6bcd2a92e73dbc8456
Juan Pablo Alfonzo <juanpabloalfonzo@gmail.com>
2020-11-09 20:43:38 -0500

    Troubleshooting Classification Condition

--- SQLSearchGraph.py
+++ SQLSearchGraph.py
@@ -29,7 +29,7 @@ ha_flux=data1[:,1]
 ha_flux=np.delete(ha_flux,0)
 
 #Creating x points to allow the plotting of the distinguishing line
-x1=np.linspace(7,13,9719)
+x1=np.linspace(7,13,9718)
 y1=seperationline(x1)
 
 
@@ -39,25 +39,35 @@ plt.ylabel('Log of SFR')
 
 #plt.scatter(logmass,logSFR, c=np.log10(ha_flux), vmin=-2, vmax=-0.8, cmap='viridis', alpha=0.1)
 #plt.hist2d(logmass,logSFR, cmap='viridis', bins=(np.linspace(7,13,51),np.linspace(-5.5,1,51)))
-# plt.plot(x1,y1)
-# plt.colorbar()
+#plt.plot(x1,y1)
+#plt.colorbar()
 # plt.savefig('Distinction Line.png')
-# plt.show()
+#plt.show()
 
 #Putting Manga ID and logSFR/logmass in one array so galaxies can be tracked
-logSFR_ID=np.column_stack((np.transpose(mangaid),np.transpose(logSFR)))
-logmass_ID=np.column_stack((np.transpose(mangaid),np.transpose(logmass)))
+logSFR_ID=np.column_stack((np.transpose(mangaid),np.transpose(logSFR),np.transpose(logmass)))
+#logmass_ID=np.column_stack((np.transpose(mangaid),np.transpose(logmass)))
 
 # Removing all rows with a NaN and infinity elments in logSFR and logmass to avoid problems with np.where
 
 logSFR_ID=logSFR_ID[~np.isnan(logSFR_ID).any(axis=1)]
 logSFR_ID=logSFR_ID[~np.isinf(logSFR_ID).any(axis=1)]
 
-logmass_ID=logmass_ID[~np.isnan(logmass_ID).any(axis=1)]
-logmass_ID=logmass_ID[~np.isinf(logmass_ID).any(axis=1)]
+# logmass_ID=logmass_ID[~np.isnan(logmass_ID).any(axis=1)]
+# logmass_ID=logmass_ID[~np.isinf(logmass_ID).any(axis=1)]
 
 # Using np.where to find galaxy information around the distinction line
-# QG= np.delete(logSFR_ID, np.nonzero( np.bitwise_and( (logSFR_ID[:,1]>y1[0]), (logSFR_ID[:,1] == y1[0]) ))[0], 0)
-QG = logSFR_ID[np.logical_not(np.logical_and(logSFR_ID[:,1] < y1[0], logSFR_ID[:,1] < y1[0]))]
+#QG = logSFR_ID[np.logical_not(np.logical_and(logSFR_ID[:,1] < y1[0], logSFR_ID[:,1] < y1[0]))]
+QG=logSFR_ID[~((logSFR_ID[:,1]>y1))]
+
+SFG=logSFR_ID[~((logSFR_ID[:,1]<y1))]
 
 np.savetxt('Quiescent Galaxies From Distinction Line',QG)
+np.savetxt('Star-Forming Galaxies From Distinction Line',SFG)
+
+#Checking if Classification above and below line actually worked
+plt.scatter(SFG[:,2],SFG[:,1],marker='.')
+plt.scatter(QG[:,2],QG[:,1],marker='s')
+plt.xlim(6,14)
+plt.plot(x1,y1)
+plt.show()
